40ebd78ac1cd05aa9212270a5aaa6a044e17fe6c
Michael Wallin <michael.wallin@webtrends.com>
2015-06-04 15:21:17 -0700

    Changes to allow websocket endpoint paths to be parsed for parameters like Command paths

--- components/wookiee-spray/src/main/scala/com/webtrends/harness/component/spray/websocket/CoreWebSocketWorker.scala
+++ components/wookiee-spray/src/main/scala/com/webtrends/harness/component/spray/websocket/CoreWebSocketWorker.scala
@@ -1,21 +1,25 @@
 package com.webtrends.harness.component.spray.websocket
 
-import java.net.URLEncoder
-
+import akka.pattern.ask
 import akka.actor.{ActorRef, Props}
+import com.webtrends.harness.command.CommandBean
 import com.webtrends.harness.health.ActorHealth
 import spray.can.server.UHttp
 import spray.can.websocket.frame.TextFrame
 import spray.can.{Http, websocket}
 import spray.routing.HttpServiceActor
 
+import scala.util.{Failure, Success}
+
 /**
  * Created by wallinm on 3/27/15.
  */
 class CoreWebSocketWorker(val serverConnection: ActorRef) extends HttpServiceActor with ActorHealth with websocket.WebSocketServerWorker {
+  import scala.concurrent.ExecutionContext.Implicits.global
   override def receive = health orElse handshaking orElse closeLogic
 
   var worker: Option[ActorRef] = None
+  var bean: Option[CommandBean] = None
 
   override def handshaking: Receive = {
     // when a client request for upgrading to websocket comes in, we send
@@ -23,11 +27,11 @@ class CoreWebSocketWorker(val serverConnection: ActorRef) extends HttpServiceAct
     case websocket.HandshakeRequest(state) =>
       // Check to see if the uri has a registered worker
       // if not close the connection
-      val path = state.request.uri.path.tail
-      val props = WebSocketManager.getWorker(path.toString())
-      props match {
-        case Some(p) =>
-          worker = Some(context.actorOf(p, URLEncoder.encode(path.toString(), "UTF-8")))
+      val path = state.request.uri.path
+      WebSocketManager.getWorker(path.toString()) match {
+        case Some((props, b)) =>
+          bean = b
+          worker = Some(context.actorOf(props, props.actorClass().getSimpleName))
           state match {
             case wsFailure: websocket.HandshakeFailure => sender() ! wsFailure.response
             case wsContext: websocket.HandshakeContext => sender() ! UHttp.UpgradeServer(websocket.pipelineStage(self, wsContext), wsContext.response)
@@ -39,8 +43,19 @@ class CoreWebSocketWorker(val serverConnection: ActorRef) extends HttpServiceAct
 
     // upgraded successfully
     case UHttp.Upgraded =>
-      context.become(health orElse businessLogic orElse closeLogic)
-      self ! websocket.UpgradedToWebSocket // notify Upgraded to WebSocket protocol
+    worker match {
+      case Some(w) =>
+        w.ask(SetBean(bean)) onComplete {
+          case Success (s) =>
+            context.become(health orElse businessLogic orElse closeLogic)
+            self ! websocket.UpgradedToWebSocket // notify Upgraded to WebSocket protocol
+          case Failure (f) =>
+            context.stop (self)
+        }
+      case None =>
+        log.error(s"Websocket upgraded with no worker, closing connection")
+        context.stop(self)
+    }
   }
 
   def businessLogic: Receive = {
--- components/wookiee-spray/src/main/scala/com/webtrends/harness/component/spray/websocket/WebSocketWorker.scala
+++ components/wookiee-spray/src/main/scala/com/webtrends/harness/component/spray/websocket/WebSocketWorker.scala
@@ -1,10 +1,14 @@
 package com.webtrends.harness.component.spray.websocket
 
 import com.webtrends.harness.app.HActor
+import com.webtrends.harness.command.CommandBean
 import spray.can.{websocket, Http}
 import spray.can.websocket.FrameCommandFailed
 
+import scala.util.Success
+
 final case class Push(msg: String)
+final case class SetBean(bean: Option[CommandBean])
 
 /**
  * Created by wallinm on 4/3/15.
@@ -13,10 +17,16 @@ trait WebSocketWorker extends HActor {
 
   override def receive = health orElse businessLogic orElse closeLogic
 
+  var bean: Option[CommandBean] = None
+
   def businessLogic: Receive = {
     case Push(msg) =>
       log.debug("Got message " + msg)
       context.parent ! Push(msg)
+
+    case SetBean(b) =>
+      bean = b
+      sender() ! Success
 
     case x: FrameCommandFailed =>
       log.error("Server frame command failed", x)
