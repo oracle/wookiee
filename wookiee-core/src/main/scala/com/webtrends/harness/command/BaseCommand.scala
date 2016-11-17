c35ab0e7fbdcaecb9e7da9c0374f064d392337d4
Ladinu Chandrasinghe <ladinu.chandrasinghe@webtrends.com>
2016-11-17 11:38:57 -0800

    renamed trait

--- wookiee-core/src/main/scala/com/webtrends/harness/command/BaseCommand.scala
+++ wookiee-core/src/main/scala/com/webtrends/harness/command/BaseCommand.scala
@@ -4,7 +4,7 @@ import com.webtrends.harness.logging.Logger
 
 import scala.concurrent.Future
 
-trait CommandLike {
+trait BaseCommand {
   protected val log : Logger
   def path: String
   def execute[T:Manifest](bean: Option[CommandBean]) : Future[BaseCommandResponse[T]]
