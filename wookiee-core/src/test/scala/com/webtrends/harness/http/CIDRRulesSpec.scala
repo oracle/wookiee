76b61e9bc8e962c7da30df329c004ed99bac9e6c
Spencer Wood <spencer.wood@oracle.com>
2017-08-29 15:48:26 -0700

    Change CIDR rules to accept ipv6

---
+++ wookiee-core/src/test/scala/com/webtrends/harness/http/CIDRRulesSpec.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015 Webtrends (http://www.webtrends.com)
+ *
+ * See the LICENCE.txt file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.webtrends.harness.http
+
+import java.net.{Inet4Address, InetAddress}
+
+import com.webtrends.harness.authentication.CIDRRules
+import org.specs2.mutable.SpecificationWithJUnit
+
+class CIDRRulesSpec extends SpecificationWithJUnit {
+  sequential
+
+  "CIDRRules " should {
+    "allow ipv4 requests through" in {
+      val rules = CIDRRules(Seq("127.0.0.1/30"), Seq())
+      rules.checkCidrRules(InetAddress.getByName("127.0.0.1")) equals true
+    }
+
+    "deny ipv4 requests on wrong address" in {
+      val rules = CIDRRules(Seq("127.0.0.1/30"), Seq())
+      rules.checkCidrRules(InetAddress.getByName("15.12.13.12")) equals false
+    }
+
+    "deny ipv4 requests in the deny subnet" in {
+      val rules = CIDRRules(Seq("127.0.0.1/30"), Seq("127.10.0.1/30"))
+      rules.checkCidrRules(InetAddress.getByName("127.10.0.1")) equals false
+    }
+
+    "allow ipv6 requests through" in {
+      val rules = CIDRRules(Seq("1:0:0:0:0:0:0:1"), Seq())
+      rules.checkCidrRules(InetAddress.getByName("1:0:0:0:0:0:0:1")) equals true
+    }
+
+    "deny ipv6 requests in the deny subnet" in {
+      val rules = CIDRRules(Seq("1:0:0:0:0:0:0:1"), Seq("1:0:0:0:0:5:0:1"))
+      rules.checkCidrRules(InetAddress.getByName("1:0:0:0:0:5:0:1")) equals false
+    }
+  }
+}
+
