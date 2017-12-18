cafb9ca5d6b966279307a5bf77fb7ac0a96dc1d9
Spencer Wood <spencer.wood@oracle.com>
2017-12-18 15:58:12 -0800

    Make all vals into defs

--- wookiee-test/src/main/scala/com/webtrends/harness/service/test/BaseSpecTest.scala
+++ wookiee-test/src/main/scala/com/webtrends/harness/service/test/BaseSpecTest.scala
@@ -1,7 +1,7 @@
 package com.webtrends.harness.service.test
 
 import ch.qos.logback.classic.Level
-import com.typesafe.config.Config
+import com.typesafe.config.{Config, ConfigFactory}
 import com.webtrends.harness.component.Component
 import com.webtrends.harness.service.Service
 import org.specs2.mutable.SpecificationLike
@@ -10,9 +10,9 @@ import org.specs2.mutable.SpecificationLike
  * @author Michael Cuthbert on 5/28/15.
  */
 trait BaseSpecTest extends SpecificationLike {
-  implicit val config:Config
-  implicit val componentMap:Option[Map[String, Class[_<:Component]]] = None
-  implicit val servicesMap:Option[Map[String, Class[_<:Service]]] = None
+  def config:Config = ConfigFactory.empty()
+  def componentMap:Option[Map[String, Class[_<:Component]]] = None
+  def servicesMap:Option[Map[String, Class[_<:Service]]] = None
 
   TestHarness(config, servicesMap, componentMap, Level.ALL)
   Thread.sleep(1000)
