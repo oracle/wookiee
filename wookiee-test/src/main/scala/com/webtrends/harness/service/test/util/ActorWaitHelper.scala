e80755eadf3678b50fb2c657a879cf5de7e7617b
Spencer Wood <spencer.wood@oracle.com>
2018-01-04 16:29:28 -0800

    Logging and Waiting Improvements * NPE fix on shutdown * Helper logging method and spec to log on failure * Await actor by ref

--- wookiee-test/src/main/scala/com/webtrends/harness/service/test/util/ActorWaitHelper.scala
+++ wookiee-test/src/main/scala/com/webtrends/harness/service/test/util/ActorWaitHelper.scala
@@ -9,8 +9,16 @@ import scala.concurrent.Await
 
 object ActorWaitHelper {
   // Will wait until an actor has come up before returning its ActorRef
-  def awaitActor(props: Props, system: ActorSystem)(implicit timeout: Timeout = Timeout(5, TimeUnit.SECONDS)): ActorRef = {
-    val actor = system.actorOf(props)
+  def awaitActor(props: Props, system: ActorSystem, actorName: Option[String] = None)(implicit timeout: Timeout = Timeout(5, TimeUnit.SECONDS)): ActorRef = {
+    val actor = actorName match {
+      case Some(name) => system.actorOf(props, name)
+      case None => system.actorOf(props)
+    }
+    awaitActorRef(actor, system)
+  }
+
+  // Will wait until an actor has come up before returning its ActorRef
+  def awaitActorRef(actor: ActorRef, system: ActorSystem)(implicit timeout: Timeout = Timeout(5, TimeUnit.SECONDS)): ActorRef = {
     Await.result(system.actorSelection(actor.path).resolveOne(), timeout.duration)
     actor
   }
