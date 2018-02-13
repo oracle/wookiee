9656d9c8820b7f34c7b8ba1fc6c461e60a550088
Jeff Berger <jberger25@yahoo.com>
2018-02-12 16:05:12 -0800

    Log command execution times (#124)

--- wookiee-core/src/main/scala/com/webtrends/harness/command/typed/TypedCommand.scala
+++ wookiee-core/src/main/scala/com/webtrends/harness/command/typed/TypedCommand.scala
@@ -3,8 +3,9 @@ package com.webtrends.harness.command.typed
 import akka.actor.Actor
 import akka.pattern._
 import com.webtrends.harness.logging.ActorLoggingAdapter
-
+import com.webtrends.harness.utils.FutureExtensions._
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 trait TypedCommand[T, V] extends Actor with ActorLoggingAdapter {
 
@@ -13,7 +14,17 @@ trait TypedCommand[T, V] extends Actor with ActorLoggingAdapter {
   def commandName: String
 
   def receive: Receive = {
-    case ExecuteTypedCommand(args) => pipe(execute(args.asInstanceOf[T])) to sender
+    case ExecuteTypedCommand(args) => pipe{
+      val startTime = System.currentTimeMillis()
+      execute(args.asInstanceOf[T]) mapAll {
+        case Success(t) =>
+          log.info(s"Command $commandName succeeded in ${System.currentTimeMillis() - startTime}ms")
+          t
+        case Failure(f) =>
+          log.info(s"Command $commandName failed in ${System.currentTimeMillis() - startTime}ms")
+          throw f
+      }
+    } to sender
   }
 
   def execute(args: T): Future[V]
