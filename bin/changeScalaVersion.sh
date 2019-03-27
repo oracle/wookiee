27e51ff580840c6088ca95146bbbad0e6490eae2
Spencer Wood <spencer.wood@oracle.com>
2019-03-27 12:02:47 -0500

    Script to change scala versions, instead of convoluted property overrides

---
+++ bin/changeScalaVersion.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "Changing Scala Version to $1, and Scala Artifact Version to $2"
+
+sed -i "s/<scala\.version>.*</<scala\.version>$1</" pom.xml
+sed -i "s/<scala\.artifact\.version>.*</<scala\.artifact\.version>$2</" pom.xml
