f7937a72fb01aefef90e0511c3a3e0496d0beb3e
Spencer Wood <spencer.wood@oracle.com>
2019-12-31 18:44:29 -0600

    Allow Override of Test Harness Startup Wait (#150)
    * Fix for Broken Pipe error from SimpleHttpServer
    * Will caused by failing to catch an IOException
    
    * One extra Try to prevent an NPE on shutdown
    
    * Package name change
    
    * Safe check for when context is null
    
    * Config file change to debug
    
    * Change TestHarness to allow one to input their own timeout for startup checks
    
    * Pass up time to wait
    
    * Allow one to increase the startup wait for Wookiee Test

--- wookiee-test/src/main/scala/com/webtrends/harness/service/test/BaseSpecTest.scala
+++ wookiee-test/src/main/scala/com/webtrends/harness/service/test/BaseSpecTest.scala
@@ -1,5 +1,6 @@
 package com.webtrends.harness.service.test
 
+import akka.actor.ActorSystem
 import ch.qos.logback.classic.Level
 import com.typesafe.config.{Config, ConfigFactory}
 import com.webtrends.harness.component.Component
@@ -7,15 +8,18 @@ import com.webtrends.harness.service.Service
 import org.specs2.mutable.SpecificationLike
 import org.scalatest.{MustMatchers, WordSpecLike}
 
+import scala.concurrent.duration._
+
 trait BaseWookieeTest {
   def config:Config = ConfigFactory.empty()
   def componentMap:Option[Map[String, Class[_<:Component]]] = None
   def servicesMap:Option[Map[String, Class[_<:Service]]] = None
   def logLevel: Level = Level.INFO
+  def startupWait: FiniteDuration = 15 seconds
 
-  TestHarness(config, servicesMap, componentMap, logLevel)
+  TestHarness(config, servicesMap, componentMap, logLevel, startupWait)
   Thread.sleep(1000)
-  implicit val system = TestHarness.system.get
+  implicit val system: ActorSystem = TestHarness.system.get
 }
 
 trait BaseWookieeSpecTest extends BaseWookieeTest with SpecificationLike
