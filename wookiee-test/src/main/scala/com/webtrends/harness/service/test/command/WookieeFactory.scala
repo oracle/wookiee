c89b7743a62be5b5efe83fb7ac2e7e38f37c2cd3
Spencer Wood <spencer.wood@oracle.com>
2020-04-01 12:03:49 -0500

    New signature for marshaller support

--- wookiee-test/src/main/scala/com/webtrends/harness/service/test/command/WookieeFactory.scala
+++ wookiee-test/src/main/scala/com/webtrends/harness/service/test/command/WookieeFactory.scala
@@ -1,21 +1,39 @@
 package com.webtrends.harness.service.test.command
 
 import akka.actor.Props
-import com.webtrends.harness.command.Command
+import com.webtrends.harness.command.{Bean, Command}
 
 import scala.concurrent.Future
 import scala.reflect.ClassTag
 
 object WookieeFactory {
   def createWookieeCommand[U <: Product: ClassTag, V: ClassTag](
-                                                      name: String,
-                                                      bodyClass: Class[U],
+                                                      bodyClass: Class[U], // TODO Where to use?
                                                       businessLogic: U => Future[V]
                                                     ): Props = {
     class NewWookiee extends Command[U, V] {
       override def execute(bean: U): Future[V] = {
-        print(s"Triggered $name")
         businessLogic(bean)
+      }
+    }
+    object NewWookiee {
+      def apply() = new NewWookiee()
+    }
+    Props({NewWookiee()})
+  }
+
+  // TODO: what about simple commands that don't return anything, do they need a custom unmarshaller?
+  def createWookieeCommand[U <: Product: ClassTag, V: ClassTag](
+     customUnmarshaller: Bean => U,
+     businessLogic: U => Future[V],
+     customMarshaller: V => Array[Byte]): Props = {
+
+    class NewWookiee extends Command[Bean, Array[Byte]] {
+      import context.dispatcher
+
+      override def execute(bean: Bean): Future[Array[Byte]] = {
+        val input = customUnmarshaller(bean)
+        businessLogic(input) map customMarshaller
       }
     }
     object NewWookiee {
