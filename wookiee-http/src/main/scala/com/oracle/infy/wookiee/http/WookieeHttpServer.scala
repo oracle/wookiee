a1fb2819c46592be387682322ad6fa2af73ff740
Ladinu Chandrasinghe <ladinu.chandrasinghe@oracle.com>
2022-05-12 14:54:15 -0700

    Cats upgrade (#202)
    Upgrade all cats ecosystem libs

--- wookiee-http/src/main/scala/com/oracle/infy/wookiee/http/WookieeHttpServer.scala
+++ wookiee-http/src/main/scala/com/oracle/infy/wookiee/http/WookieeHttpServer.scala
@@ -1,21 +1,24 @@
 package com.oracle.infy.wookiee.http
 
-import cats.effect.{ContextShift, ExitCode, IO, Timer}
+import cats.effect.{ExitCode, IO}
 import fs2.Stream
 import org.http4s.HttpRoutes
+import org.http4s.blaze.server.BlazeServerBuilder
 import org.http4s.implicits._
-import org.http4s.server.blaze._
 
 import scala.concurrent.ExecutionContext
 
 object WookieeHttpServer {
 
-  def of(host: String, port: Int, httpRoutes: HttpRoutes[IO], executionContext: ExecutionContext)(
-      implicit timer: Timer[IO],
-      cs: ContextShift[IO]
+  def of(
+      host: String,
+      port: Int,
+      httpRoutes: HttpRoutes[IO],
+      executionContext: ExecutionContext
   ): Stream[IO, ExitCode] = {
     val httpApp = httpRoutes.orNotFound
-    BlazeServerBuilder[IO](executionContext)
+    BlazeServerBuilder[IO]
+      .withExecutionContext(executionContext)
       .bindHttp(port, host)
       .withoutBanner
       .withHttpApp(httpApp)
