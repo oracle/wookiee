ea807197ee90a9a64e9c9cab2b9b66508f1777db
Ladinu Chandrasinghe <ladinu.chandrasinghe@oracle.com>
2022-06-13 15:31:58 -0700

    Add gauge and fix WookieeMetricsNoOpImpl (#207)
    * Add gauge and fix WookieeMetricsNoOpImpl
    
    * Update version

--- wookiee-functional-metrics/src/main/scala/com/oracle/infy/wookiee/functional/metrics/core/WookieeMetrics.scala
+++ wookiee-functional-metrics/src/main/scala/com/oracle/infy/wookiee/functional/metrics/core/WookieeMetrics.scala
@@ -1,21 +1,21 @@
 package com.oracle.infy.wookiee.functional.metrics.core
 
-import com.oracle.infy.wookiee.metrics.model.{Counter, Gauge, Histogram, Meter, Timer}
+import com.oracle.infy.wookiee.metrics.model._
 import io.circe.Json
 
 trait WookieeMetrics[F[_]] {
 
-  def timer(name: String): F[Timer]
+  def timer(name: String): F[Timer[F]]
 
   def time[A](name: String)(inner: F[A]): F[A]
 
-  def counter(name: String): F[Counter]
+  def counter(name: String): F[Counter[F]]
 
-  def meter(name: String): F[Meter]
+  def meter(name: String): F[Meter[F]]
 
-  def histogram(name: String, biased: Boolean): F[Histogram]
+  def histogram(name: String, biased: Boolean): F[Histogram[F]]
 
-  def gauge[A](name: String, f: () => A): F[Gauge[A]]
+  def gauge[A](name: String): F[Gauge[F, A]]
 
   def remove(name: String): F[Boolean]
 
