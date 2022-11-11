17f48c189675a4c4eab10691ef87360c10a314c3
dheeraj <dheeraj.tallam@oracle.com>
2022-11-11 10:50:34 +0530

    updating  timeout

--- wookiee-core/src/main/scala/com/oracle/infy/wookiee/app/HarnessActor.scala
+++ wookiee-core/src/main/scala/com/oracle/infy/wookiee/app/HarnessActor.scala
@@ -286,7 +286,7 @@ class HarnessActor extends Actor with ActorLoggingAdapter with Health with Confi
       // Call the sections and get their health
       val future = Future.traverse(context.children) { a: ActorRef =>
         log.info("Getting the actor ref ::" + a.path.name)
-        val healthComponent = (a ? CheckHealth)(5.second).mapTo[HealthComponent]
+        val healthComponent = (a ? CheckHealth)(3.second).mapTo[HealthComponent]
         log.info("checking the health component :: " + healthComponent.value)
         healthComponent
       }
