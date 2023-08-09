10403a9d479bf41de80dd43690cede0186113fd3
Spencer Wood <spencer.wood@oracle.com>
2023-08-09 11:34:21 -0500

    Override arity methods for java interop since it doesn't automatically define them

--- wookiee-libs/src/main/scala/com/oracle/infy/wookiee/component/ComponentV2.scala
+++ wookiee-libs/src/main/scala/com/oracle/infy/wookiee/component/ComponentV2.scala
@@ -20,4 +20,21 @@ case class ComponentV2(override val name: String, override val config: Config)
   // When someone sends a ComponentMessage to this component, this method will be called
   // Nothing will wait on a result from this method
   def onMessage[T](msg: T): Unit = self ! msg
+
+  /* Java Interop */
+
+  override def canEqual(that: Any): Boolean = that match {
+    case _: ComponentV2 => true
+    case _              => false
+  }
+
+  override def productElement(n: Int): Any = {
+    n match {
+      case 0 => name
+      case 1 => config
+      case _ => throw new IndexOutOfBoundsException(n.toString)
+    }
+  }
+
+  override def productArity: Int = 2
 }
--- wookiee-libs/src/main/scala/com/oracle/infy/wookiee/service/ServiceV2.scala
+++ wookiee-libs/src/main/scala/com/oracle/infy/wookiee/service/ServiceV2.scala
@@ -6,4 +6,20 @@ import com.typesafe.config.Config
 
 abstract case class ServiceV2(config: Config) extends WookieeService with WookieeActor with ConfigHelperV2 {
   override var renewableConfig: Config = config
+
+  /* Java Interop */
+
+  override def canEqual(that: Any): Boolean = that match {
+    case _: ServiceV2 => true
+    case _            => false
+  }
+
+  override def productElement(n: Int): Any = {
+    n match {
+      case 0 => config
+      case _ => throw new IndexOutOfBoundsException(n.toString)
+    }
+  }
+
+  override def productArity: Int = 1
 }
